49e1015b498b5d815e0d01c727171aaf8261b40e
Gabriel Faria <gabrielfaria88@gmail.com>
2023-10-10 09:49:33 -0300

    persistindo dados localStorage

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,17 @@
 const taskListContainer = document.querySelector('.app__section-task-list');
-
 const formTask = document.querySelector('.app__form-add-task');
 const toggleFormTaskBtn = document.querySelector('.app__button--add-task');
 const formLabel = document.querySelector('.app__form-label');
-const closeForm = document.querySelector('.app__form-footer__button--cancel');
-
 const textArea = document.querySelector('.app__form-textarea');
-
-let tarefas = []
-
 const taskIconSvg = '<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="12" cy="12" r="12" fill="#FFF" /> <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.17192" fill="#01080E"/> </svg>'
+const closeForm = document.querySelector('.app__form-footer__button--cancel');
+const clearForm = () => {
+    textArea.value = "";
+}
+const localStorageTasks = localStorage.getItem('tarefas');
+
+let tarefas = localStorageTasks ? JSON.parse(localStorageTasks) : []
+
 
 function createTask(tarefa) {
     const li = document.createElement('li')
@@ -38,7 +40,12 @@ toggleFormTaskBtn.addEventListener('click', () => {
     formLabel.textContent = 'Adicionando tarefa'
     formTask.classList.toggle('hidden')
     toggleFormTaskBtn.classList.toggle('hidden');
+    textArea.focus();
 })
+
+const updateLocalStorage = () => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
@@ -49,11 +56,14 @@ formTask.addEventListener('submit', (evento) => {
     tarefas.push(task)
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
-    textArea.value = "";
+    updateLocalStorage();
+    clearForm();
     textArea.focus();
 })
 
 closeForm.addEventListener('click', () => {
     formTask.classList.toggle('hidden')
     toggleFormTaskBtn.classList.toggle('hidden');
+    clearForm();
 })
+
