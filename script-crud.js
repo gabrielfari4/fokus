820c5e279b59f3ffa14f058dd74470b59db72dc8
Gabriel Faria <gabrielfaria88@gmail.com>
2023-10-10 12:11:38 -0300

    implementando ediçao de tarefas

--- script-crud.js
+++ script-crud.js
@@ -6,6 +6,8 @@ const textArea = document.querySelector('.app__form-textarea');
 const taskIconSvg = '<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="12" cy="12" r="12" fill="#FFF" /> <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.17192" fill="#01080E"/> </svg>'
 const closeForm = document.querySelector('.app__form-footer__button--cancel');
 const clearForm = () => {
+    taskToEdit = null;
+    paragraphToEdit = null;
     textArea.value = "";
 }
 const localStorageTasks = localStorage.getItem('tarefas');
@@ -15,6 +17,22 @@ let tarefas = localStorageTasks ? JSON.parse(localStorageTasks) : []
 
 let selectedTask = null;
 let selectedTaskItem = null;
+
+let taskToEdit = null;
+let paragraphToEdit = null;
+
+const selectingTaskToEdit = (tarefa, elemento) => {
+    if (taskToEdit == tarefa) {
+        clearForm();
+        return
+    }
+
+    formLabel.textContent = "Editando tarefa"
+    taskToEdit = tarefa
+    paragraphToEdit = elemento
+    textArea.value = tarefa.descricao
+    formTask.classList.remove('hidden')
+}
 
 const selectTask = (tarefa, elemento) => {
     document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
@@ -46,12 +64,38 @@ function createTask(tarefa) {
 
     paragraph.textContent = tarefa.descricao
 
+    const button = document.createElement('button')
+
+    button.classList.add('app_button-edit')
+    const editIcon = document.createElement('img')
+    editIcon.setAttribute('src', '/imagens/edit.png')
+
+    button.appendChild(editIcon)
+
+    button.addEventListener('click', (event) => {
+        event.stopPropagation();
+        selectingTaskToEdit(tarefa, paragraph)
+    })
+
     li.onclick = () => {
         selectTask(tarefa, li);
     }
 
+    svgIcon.addEventListener('click', (event) => {
+        event.stopPropagation()
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    })
+
+    if (tarefa.concluida) {
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    }
+
+    
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
+    li.appendChild(button)
 
     return li
 }
@@ -74,13 +118,19 @@ const updateLocalStorage = () => {
 
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
-    const task = {
-        descricao: textArea.value,
-        concluida: false
+    if (taskToEdit) {
+        taskToEdit.descricao = textArea.value
+        paragraphToEdit.textContent = textArea.value
+    } else {
+        const task = {
+            descricao: textArea.value,
+            concluida: false
+        }
+        tarefas.push(task)
+        const taskItem = createTask(task)
+        taskListContainer.appendChild(taskItem)
+        
     }
-    tarefas.push(task)
-    const taskItem = createTask(task)
-    taskListContainer.appendChild(taskItem)
     updateLocalStorage();
     clearForm();
     textArea.focus();
