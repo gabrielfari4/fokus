ba0d8aa2a36fe49c8b106a2154252af3f4e374d7
Gabriel Faria <gabrielfaria88@gmail.com>
2023-10-10 09:20:38 -0300

    implementando taskform

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,13 @@
 const taskListContainer = document.querySelector('.app__section-task-list');
 
-let tarefas = [
-    {
-        descricao: 'Tarefa Concluída',
-        concluida: true
-    },
-    {
-        descricao: 'Tarefa Pendente',
-        concluida: false
-    }
-]
+const formTask = document.querySelector('.app__form-add-task');
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task');
+const formLabel = document.querySelector('.app__form-label');
+const closeForm = document.querySelector('.app__form-footer__button--cancel');
+
+const textArea = document.querySelector('.app__form-textarea');
+
+let tarefas = []
 
 const taskIconSvg = '<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="12" cy="12" r="12" fill="#FFF" /> <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.17192" fill="#01080E"/> </svg>'
 
@@ -34,4 +32,28 @@ function createTask(tarefa) {
 tarefas.forEach(task => {
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
+})
+
+toggleFormTaskBtn.addEventListener('click', () => {
+    formLabel.textContent = 'Adicionando tarefa'
+    formTask.classList.toggle('hidden')
+    toggleFormTaskBtn.classList.toggle('hidden');
+})
+
+formTask.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const task = {
+        descricao: textArea.value,
+        concluida: false
+    }
+    tarefas.push(task)
+    const taskItem = createTask(task)
+    taskListContainer.appendChild(taskItem)
+    textArea.value = "";
+    textArea.focus();
+})
+
+closeForm.addEventListener('click', () => {
+    formTask.classList.toggle('hidden')
+    toggleFormTaskBtn.classList.toggle('hidden');
 })
